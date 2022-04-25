c6c09ae04505826c4120757030a3a627d626c74f
Karim Ben Ammar <29929026+karimbenammar@users.noreply.github.com>
2022-04-25 13:07:16 +0100

    fix: update method documentation

--- app/src/main/java/com/yassir/movies/util/PicassoHelper.kt
+++ app/src/main/java/com/yassir/movies/util/PicassoHelper.kt
@@ -7,9 +7,12 @@ import com.squareup.picasso.Picasso
 object PicassoHelper {
 
     /**
-     * Appends genres text and returns a full string containing genres separated by commas
+     * Loads an image from url into a view
      *
-     * @param genres list of genres
+     * @param url url of the image to be loaded
+     * @param imageView view that will contain the image
+     * @param placeholderId resource id of the placeholder to display
+     * @param callback callback to be executed after loading the image
      */
     fun loadImage(url: String?, imageView: ImageView, placeholderId: Int, callback: Callback? = null) {
         val picassoRequest = if (url.isNullOrEmpty())
